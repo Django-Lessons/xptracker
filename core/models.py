bbd850d594fd81d4e4d5a663ade536bf94ab055d
Eugen Ciur <eugen@django-lessons.com>
2020-06-08 18:31:54 +0200

    add models

--- core/models.py
+++ core/models.py
@@ -1,6 +1,46 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class XpAccount(models.Model):
+    title = models.CharField(
+        max_length=64
+    )
+    description = models.TextField()
+    amount = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE
+    )
+
+
+class XpTransaction(models.Model):
+
+    created_at = models.DateField()
+    description = models.TextField()
+    amount = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
+    src = models.ForeignKey(
+        'XpAccount',
+        on_delete=models.CASCADE,
+        related_name='src'
+    )
+    dst = models.ForeignKey(
+        'XpAccount',
+        on_delete=models.CASCADE,
+        related_name='dst'
+    )
+
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE
+    )
