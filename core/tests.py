bd57d2d0a730e18a02ef192122cb97530f20e8fa
Eugen Ciur <eugen@django-lessons.com>
2020-06-08 19:02:58 +0200

    basic transaction

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,56 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User, XpAccount
+from .utils import perform_transaction
+
+
+class XPTesting(TestCase):
+
+    def setUp(self):
+        self.user = User(username='john')
+        self.user.save()
+
+    def test_basics(self):
+        """
+        create two accounts and perform a transaction
+        """
+        groceries = XpAccount.objects.create(
+            title="Groceries",
+            amount=0.0,
+            user=self.user
+        )
+        self.assertEqual(
+            1,
+            XpAccount.objects.count(),
+        )
+        bank_account = XpAccount.objects.create(
+            title="BankAccount",
+            amount=1000.0,
+            user=self.user
+        )
+        self.assertEqual(
+            2,
+            XpAccount.objects.count(),
+        )
+        self.assertEqual(
+            groceries.amount,
+            0.0
+        )
+        self.assertEqual(
+            bank_account.amount,
+            1000.0
+        )
+        perform_transaction(
+            src=bank_account,
+            dst=groceries,
+            amount=37.29,
+            description="Groceries at MegaDiscounter"
+        )
+        self.assertEqual(
+            groceries.amount,
+            37.29
+        )
+        self.assertEqual(
+            bank_account.amount,
+            962.71
+        )
---
+++ core/utils.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+from .models import XpTransaction
+
+
+def perform_transaction(src, dst, amount, description):
+    """
+    Move given amount of money from source to destination
+    account
+    """
+    XpTransaction.objects.create(
+        src=src,
+        dst=dst,
+        amount=amount,
+        description=description,
+        user=src.user,
+        created_at=date.today()
+    )
+    src.amount -= amount
+    src.save()
+    dst.amount += amount
+    dst.save()
