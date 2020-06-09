db8c3c168bd95c8bd81cbfb5c465bea8822b5ecd
Eugen Ciur <eugen@django-lessons.com>
2020-06-09 09:23:02 +0200

    adding basic xpaccount model

--- core/models.py
+++ core/models.py
@@ -1,6 +1,21 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class XpAccount(models.Model):
+    name = models.CharField(
+        max_length=64
+    )
+    description = models.TextField()
+    user = models.ForeignKey(
+        'User',
+        on_delete=models.CASCADE
+    )
+    amount = models.DecimalField(
+        max_digits=12,
+        decimal_places=2
+    )
