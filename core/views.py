d684489e9df360b1239a70809366554d2f6ae1d6
Eugen Ciur <eugen@django-lessons.com>
2020-06-17 13:19:33 +0200

    create new account from template

--- core/views.py
+++ core/views.py
@@ -1,7 +1,45 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
-from .models import XpAccount
+from .models import ASSET, EXPENSE, INCOME, XpAccount
+
+account_templates = [
+    {
+        'title': "Bank Account",
+        'description': "You bank account",
+        'acc_type': ASSET,
+    },
+    {
+        'title': "Income Account",
+        'description': "Account for your income like salary, consulting.",
+        'acc_type': INCOME,
+    },
+    {
+        'title': "Groceries",
+        'description': "All groceries expenses go here",
+        'acc_type': EXPENSE
+    },
+    {
+        'title': "Rent",
+        'description': "Monthly Rent",
+        'acc_type': EXPENSE
+    },
+    {
+        'title': "Insurances",
+        'description': "All kind of insurances",
+        'acc_type': EXPENSE
+    },
+    {
+        'title': "Insurances",
+        'description': "All kind of insurances",
+        'acc_type': EXPENSE
+    },
+    {
+        'title': "Utility Bills",
+        'description': "Like electricity bills, tv, internet",
+        'acc_type': EXPENSE
+    }
+]
 
 
 @login_required
@@ -21,4 +59,11 @@ def start(request):
     Help user to create first couple of accounts.
     Let him/her selects from suggested accounts
     """
-    return render(request, 'core/start.html')
+    xpaccounts = [
+        XpAccount(**acc) for acc in account_templates
+    ]
+    return render(
+        request,
+        'core/start.html',
+        {'xpaccounts': xpaccounts}
+    )
